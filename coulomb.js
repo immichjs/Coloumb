73c6438576dabbd5f4a961ab20fed4f8b0224442
Michel França <fr.mich17@gmail.com>
2020-04-08 16:19:46 -0300

    Refatoração

--- coulomb.js
+++ coulomb.js
@@ -1,9 +1,14 @@
 const formula = require('./src/formula')
 const unit = require('./src/unit')
 
-const f1 = formula.force(5, 1, 0.1),
-    f2 = formula.force(2, 1, 0.1),
+const f1 = formula.force(1, 4, 0.1),
+    f2 = formula.force(2, 4, 0.1),
     n = f1 - f2,
     e = 9 + (unit.u[1] * 2)
 
-console.log(`\n F1: ${f1}N\n F2: ${f2}N\n ${f1} - ${f2} = ${n}e${e}N = ${n * 10 ** -3}N`)
+function returnFac () {
+    return {
+        R: `F1: ${f1}N - ${f2}N = ${n}e${e}N = ${(n * 10 ** -3).toPrecision(2)}`
+    }
+}
+console.log(returnFac())
