72bd1e7060c2e9c9aeeaf9cf4e91713b70d99893
Michel França <fr.mich17@gmail.com>
2020-04-18 01:59:19 -0300

    Design update and verify value

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
-# Coloumb
+## Coloumb
 
-#### A intenção do desenvolvimento desse script, foi pra resolver uma questão de física da faculdade.
-
-- Duas cargas `Q1: 1.u C` e `Q2: 4.u C` estão fixas nos pontos A e B e separados pela distância de 20cm, no vácuo. Determine a intensidade da resulltante das forças elétricas sobre uma terceira carga `Q3: 2.u C` colocada no ponto médio do seguimento que uni `Q1 e Q2`.
-
-- Não está completa ainda, alguns erros de lógica podem estar presente caso utilizar outro tipo de questão.
+> A intenção do desenvolvimento desse script, foi pra resolver uma questão de física da faculdade.
+    - Duas cargas `Q1: 1.u C` e `Q2: 4.u C` estão fixas nos pontos A e B e separados pela distância de 20cm, no vácuo. Determine a intensidade da resulltante das forças elétricas sobre uma terceira carga `Q3: 2.u C` colocada no ponto médio do seguimento que uni `Q1 e Q2`.
--- js/funcionalidade.js
+++ js/funcionalidade.js
@@ -9,26 +9,36 @@ const resetValue = () => {
     e.distance.value = ''
 }
 
-e.btnF1.onclick = () => {
-    loadForce.push(force())
+const info = 'É preciso conter valores válidos.'
 
-    e.btnF1.classList.add('invisible')
-    e.btnF2.classList.remove('invisible')
+e.btnF1.onclick = () => {
+    if (isNaN(e.firstCharge.value || e.secondCharge.value || e.distance.value) || e.firstCharge.value == '' || e.secondCharge.value == '' || e.distance.value == '')  {
+        alert(info)
+    } else {
+        loadForce.push(force())
+
+        e.btnF1.classList.add('invisible')
+        e.btnF2.classList.remove('invisible')
+    }
 }
 
 e.btnF2.onclick = () => {
-    loadForce.push(force())
-    resetValue()
+    if (isNaN(e.firstCharge.value || e.secondCharge.value || e.distance.value) || e.firstCharge.value == '' || e.secondCharge.value == '' || e.distance.value == '')  {
+        alert(info)
+    } else {
+        loadForce.push(force())
+        resetValue()
 
-    console.log('Formula: K.|Q|.|Q| % d²')
-    console.log(loadForce)
+        console.log('Formula: K.|Q|.|Q| % d²')
+        console.log(loadForce)
 
-    const negative = [
-        loadForce[0] - loadForce[1],
-        loadForce[1] - loadForce[0],
-        (loadForce[0] - loadForce[1]) * 10 ** elevation,
-        (loadForce[1] - loadForce[0]) * 10 ** elevation,
-    ]
+        const negative = [
+            loadForce[0] - loadForce[1],
+            loadForce[1] - loadForce[0],
+            (loadForce[0] - loadForce[1]) * 10 ** elevation,
+            (loadForce[1] - loadForce[0]) * 10 ** elevation,
+        ]
 
-    console.log(`Possíveis resultantes:\n\nResultante Negativa: ${negative[0]} = ${negative[2].toPrecision(2)}N\nResultante Positiva: ${negative[1]} = ${negative[3].toPrecision(2)}N`)
+        console.log(`Possíveis resultantes:\n\nResultante Negativa: ${negative[0]} = ${negative[2].toPrecision(2)}N\nResultante Positiva: ${negative[1]} = ${negative[3].toPrecision(2)}N`)
+    }
 }
