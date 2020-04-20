ebff25203792fc6404abdeb8a4cedec61bf67b00
Michel França <53818605+immichjs@users.noreply.github.com>
2020-04-19 22:37:55 -0300

    Update script.js

--- js/controller/script.js
+++ js/controller/script.js
@@ -1,12 +1,9 @@
 import { e } from '../elements.js'
-import { force, elevation } from '../formula.js'
 import { calcOneForce } from '../operation.js'
 
 e.btn_one_force.onclick = () => {
     document.querySelector('.one-force').classList.remove('hidden')
 }
-
-let loadForce = 0
 
 e.calc_force.onclick = () => {
     calcOneForce()
